10472391f44baf6bc59871f0a175c36ac702b6be
Bing <bing.wu566@gmail.com>
2015-06-26 17:58:15 +0800

    initial commit

---
+++ WWTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WWTemplate
+//
+//  Created by Bing on 15/6/26.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WWTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WWTemplate
+//
+//  Created by Bing on 15/6/26.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
